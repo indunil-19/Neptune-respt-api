ec3a57c1f719371517968368559e4d481e9aa28e
Indunil Udayangana <indunilud@codegen.net>
2024-04-21 23:51:38 +0530

    add cors orgin

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+const corsOptions = {
+  origin: "*",
+};
+app.use(cors(corsOptions));
 const mongoose = require("mongoose");
 const { MONGOURI } = require("./config/keys");
 
