07f2fb8cd41dd44284cbe20f2ff01b3705e2eb03
Indunil Udayangana <indunilud@codegen.net>
2024-04-23 09:54:12 +0530

    add .env to get data from env file

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,6 @@
 module.exports = {
-  MONGOURI:
-    "mongodb+srv://udayangana98:NqAi5CdU69UZCfoK@cluster0.dd0piq5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-  JWT_SECRET: "sxhsscuuygxastr",
-  SENDGRID_API: "huytiufgyfd56ertrytf",
-  EMAIL: "localhost:3000",
+  MONGOURI: process.env.MONGOURI,
+  JWT_SECRET: process.env.JWT_SECRET,
+  SENDGRID_API: process.env.SENDGRID_API,
+  EMAIL: process.env.EMAIL,
 };
